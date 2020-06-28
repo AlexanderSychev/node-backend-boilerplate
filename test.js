47bf6d7bd1a330bd1cb9bae6474d47d6ec50376f
alexander <asychev@changers.team>
2020-06-28 03:49:23 +0300

    Remove test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const yaml = require('yaml');
-
-const document = yaml.parseDocument(fs.readFileSync('./config/default.yml', 'utf-8'));
-
-console.log(document.get('log').get('loggers'));
